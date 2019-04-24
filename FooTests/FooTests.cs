601d8a0b6b856d2db9f97a2f0fe3c1049c9d7a75
rvsjameswheeler <james.wheeler@radiantvs.com>
2019-04-24 15:54:50 -0700

    Remove unnecessary namespace

--- FooTests/FooTests.cs
+++ FooTests/FooTests.cs
@@ -3,79 +3,76 @@ using System.Text;
 using Moq;
 using NUnit.Framework;
 
-namespace Tests
+//TODO: create a wrapper/simple adapter around nuget SerialPortStream
+public class FooTests
 {
-    //TODO: create a wrapper/simple adapter around nuget SerialPortStream
-    public class FooTests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void ReadReturnsMessageWithMatchingHeaderAsInitialDataReceived()
-        {
-            // Arrange
-            var data = "Hello, world!";
-            byte[] bytes = Encoding.ASCII.GetBytes(data);
-
-            ITranslate translator = Mock.Of<ITranslate>(t =>
-                t.Translate(bytes) == data);
-
-            var sut = new SerialServer(translator);
-            
-            // Act
-            Message message = sut.Read(bytes);
-
-            // Assert
-            Assert.AreEqual(data, message.Header);
-        }
-
-        [Test]
-        public void ReadSuccessfullyCallsInternalSerialPortStreamReadMethod()
-        {
-            // Arrange
-            byte[] bytes = Enumerable.Empty<byte>().ToArray();
-            
-            ITranslate translator = Mock.Of<ITranslate>(t =>
-                t.Translate(bytes) == string.Empty);
-            
-            var sut = new SerialServer(translator);
-            
-            // Act
-            Message message = sut.Read(bytes);
-
-            // Assert
-            Mock.Get(translator).Verify(t => t.Translate(bytes));
-        }
     }
 
-    public interface ITranslate
+    [Test]
+    public void ReadReturnsMessageWithMatchingHeaderAsInitialDataReceived()
     {
-        string Translate(byte[] bytes);
-    }
+        // Arrange
+        var data = "Hello, world!";
+        byte[] bytes = Encoding.ASCII.GetBytes(data);
 
-    public class Message
-    {
-        public string Header { get; set; }
-    }
+        ITranslate translator = Mock.Of<ITranslate>(t =>
+            t.Translate(bytes) == data);
 
-    public class SerialServer
-    {
-        private readonly ITranslate _translator;
-
-        public SerialServer(ITranslate translator)
-        {
-            _translator = translator;
-        }
-
-        public Message Read(byte[] bytes)
-        {
-            var msg = new Message();
-            msg.Header = _translator.Translate(bytes);
+        var sut = new SerialServer(translator);
             
-            return msg;
-        }
+        // Act
+        Message message = sut.Read(bytes);
+
+        // Assert
+        Assert.AreEqual(data, message.Header);
+    }
+
+    [Test]
+    public void ReadSuccessfullyCallsInternalSerialPortStreamReadMethod()
+    {
+        // Arrange
+        byte[] bytes = Enumerable.Empty<byte>().ToArray();
+            
+        ITranslate translator = Mock.Of<ITranslate>(t =>
+            t.Translate(bytes) == string.Empty);
+            
+        var sut = new SerialServer(translator);
+            
+        // Act
+        Message message = sut.Read(bytes);
+
+        // Assert
+        Mock.Get(translator).Verify(t => t.Translate(bytes));
+    }
+}
+
+public interface ITranslate
+{
+    string Translate(byte[] bytes);
+}
+
+public class Message
+{
+    public string Header { get; set; }
+}
+
+public class SerialServer
+{
+    private readonly ITranslate _translator;
+
+    public SerialServer(ITranslate translator)
+    {
+        _translator = translator;
+    }
+
+    public Message Read(byte[] bytes)
+    {
+        var msg = new Message();
+        msg.Header = _translator.Translate(bytes);
+            
+        return msg;
     }
 }
