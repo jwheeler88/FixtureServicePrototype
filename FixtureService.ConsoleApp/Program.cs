5954c1e4691133ca7495bbb2bbccea2565c72aaa
rvsjameswheeler <james.wheeler@radiantvs.com>
2019-04-24 13:19:04 -0700

    Initial commit

---
+++ FixtureService.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FixtureService.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
