bc8e4e58a4e1b540ff5ea7e52049e9fe0f886a4e
rvsjameswheeler <james.wheeler@radiantvs.com>
2019-04-25 17:32:06 -0700

    Cleanup with Setup method

--- Rs232ServerTests/Rs232Stream_Tests/WhenCallingReadWithHelloWorldAsTheInputMessage_Test.cs
+++ Rs232ServerTests/Rs232Stream_Tests/WhenCallingReadWithHelloWorldAsTheInputMessage_Test.cs
@@ -3,40 +3,59 @@ using Moq;
 using NUnit.Framework;
 using RJCP.IO.Ports;
 
+[TestFixture("Hello, world!")]
 [Category("Rs232Stream")]
 public class WhenCallingReadWithHelloWorldAsTheInputMessage
 {
+    private readonly string _msgHeader;
+    private byte[] _buffer;
+    private ISerialPortStream _serialPort;
+    private ITranslate _translator;
+    private int _bufferSize;
+
+    public WhenCallingReadWithHelloWorldAsTheInputMessage(string msgHeader)
+    {
+        _msgHeader = msgHeader;
+    }
+    
     [SetUp]
     public void Setup()
     {
+        _bufferSize = 256;
+        _buffer = new byte[_bufferSize];
+        
+        _serialPort = GetMockSerialPort();
+        _translator = GetMockTranslator();
     }
 
     [Test]
     public void ReturnsMessageWithCorrectHeader()
     {
         // Arrange
-        var msg = "Hello, world!";
+        var sut = new Rs232Stream(_translator, _serialPort) { BufferSize = _bufferSize };
         
-        var bufferSize = 256;
-        var buffer = new byte[bufferSize];
-
-        var mockSerialPort = new Mock<ISerialPortStream>(MockBehavior.Strict);
-        mockSerialPort
-            .Setup(sp => sp.Read(buffer, 0, buffer.Length))
-            .Returns(It.IsAny<int>());
-            
-        var mockTranslator = new Mock<ITranslate>();
-        mockTranslator.Setup(t => t.Translate(buffer)).Returns(msg);
-
-        var sut = new Rs232Stream(mockTranslator.Object, mockSerialPort.Object)
-        {
-            BufferSize = bufferSize
-        };
-
         // Act
         Message message = sut.Read();
 
         // Assert
-        Assert.AreEqual(msg, message.Header);
+        StringAssert.AreEqualIgnoringCase(_msgHeader, message.Header);
+    }
+
+    private ITranslate GetMockTranslator()
+    {
+        var mockTranslator = new Mock<ITranslate>();
+        mockTranslator.Setup(t => t.Translate(_buffer)).Returns(_msgHeader);
+        
+        return mockTranslator.Object;
+    }
+
+    private ISerialPortStream GetMockSerialPort()
+    {
+        var mockSerialPort = new Mock<ISerialPortStream>(MockBehavior.Strict);
+        mockSerialPort
+            .Setup(sp => sp.Read(_buffer, 0, _buffer.Length))
+            .Returns(It.IsAny<int>());
+        
+        return mockSerialPort.Object;
     }
 }
